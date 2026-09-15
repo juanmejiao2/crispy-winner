3959b82ac69a3e44b67122950414493af9a43a7d
Juan Andres Mejia Ortiz <juan.mejiao2@udea.edu.co>
2026-09-14 21:20:34 -0500

    Creación de función para recibir las coordenadas y sobreescribir los bits en la memoria dinámica
    Se creó la función guardarFicha para poder escribir el valor de la ficha en la dirección memoria correcta

--- DESAFIO_1_CODIGO/desafio1/tablero.cpp
+++ DESAFIO_1_CODIGO/desafio1/tablero.cpp
@@ -38,5 +38,42 @@ unsigned char obtenerFicha(const unsigned char* tablero, int columnas, int fila,
 }
 
 void guardarFicha(unsigned char* tablero, int columnas, int fila, int columna, unsigned char valor){
+    valor= valor & 0x07;
+    int posicion_ficha= fila * columnas + columna;
+    int bit_inicial= posicion_ficha * 3;
+    int byte_buscado= bit_inicial / 8;
+    int desplazamiento_bit= bit_inicial % 8;
 
+    if (desplazamiento_bit<=5){
+        int desplazamiento= 5-desplazamiento_bit;
+        tablero[byte_buscado]= tablero[byte_buscado] & ~(0x07<<desplazamiento);
+        tablero[byte_buscado]=tablero[byte_buscado] | (valor<<desplazamiento);
+
+    } else if (desplazamiento_bit==6){
+        unsigned char bits_alto= (valor>>1)& 0x03;
+        unsigned char bit_bajo= valor & 0x01;
+
+        tablero[byte_buscado]= tablero[byte_buscado] & 0xFC;
+        tablero[byte_buscado]= tablero[byte_buscado] | bits_alto;
+
+        tablero[byte_buscado+1]= tablero[byte_buscado+1] & 0x7F;
+        tablero[byte_buscado+1]= tablero[byte_buscado+1] | (bit_bajo<<7);
+
+    } else {
+        unsigned char bit_alto= (valor>>2) & 0x01;
+        unsigned char bits_bajo= valor & 0x03;
+
+        tablero[byte_buscado]= tablero[byte_buscado] & 0xFE;
+        tablero[byte_buscado]= tablero[byte_buscado] | bit_alto;
+
+        tablero[byte_buscado+1]=tablero[byte_buscado+1] & 0x3F;
+        tablero[byte_buscado+1]= tablero[byte_buscado+1] | (bits_bajo<<6);
+    }
 }
+//0x07 es 00000111
+//0x7F es 01111111
+//0x3F es 00111111
+//0xFC es 11111100
+//0xFE es 11111110
+//0x03 es 00000011
+//0x01 es 00000001
