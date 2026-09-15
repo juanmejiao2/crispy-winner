9ca22374e7c8f0a288882bec459a15320531d9ee
Juan Andres Mejia Ortiz <juan.mejiao2@udea.edu.co>
2026-09-14 19:35:57 -0500

    Liberación de memoria tablero y función para obtener las fichas
    Se creó el destruir tablero para poder liberar la memoria de este mismo y se
    creó también la función para poder encontrar las fichas en el tablero, estando
    en un mismo byte y en 2 contiguos

--- DESAFIO_1_CODIGO/desafio1/tablero.cpp
+++ DESAFIO_1_CODIGO/desafio1/tablero.cpp
@@ -12,11 +12,29 @@ unsigned char* crearTablero(int filas, int columnas){
 }
 
 void destruirTablero(unsigned char* tablero){
-
+    delete[] tablero;
 }
 
 unsigned char obtenerFicha(const unsigned char* tablero, int columnas, int fila, int columna){
+    int posicion_ficha= fila * columnas + columna;
+    int bit_inicial= posicion_ficha * 3;
+    int byte_buscado= bit_inicial / 8;
+    int desplazamiento_bit= bit_inicial % 8;
 
+    unsigned char resultado= 0;
+
+    if (desplazamiento_bit<=5){
+        resultado= (tablero[byte_buscado]>>(5-desplazamiento_bit)) & 0x07;
+    } else if(desplazamiento_bit==6){
+        unsigned char bits_alto= (tablero[byte_buscado] & 0x03) << 1;
+        unsigned char bit_bajo= (tablero[byte_buscado+1] >> 7) & 0x01;
+        resultado= bits_alto | bit_bajo;
+    } else {
+        unsigned char bit_alto= (tablero[byte_buscado] & 0x01)<<2;
+        unsigned char bits_bajo= (tablero[byte_buscado+1] >> 6) & 0x03;
+        resultado= bit_alto | bits_bajo;
+    }
+    return resultado;
 }
 
 void guardarFicha(unsigned char* tablero, int columnas, int fila, int columna, unsigned char valor){
